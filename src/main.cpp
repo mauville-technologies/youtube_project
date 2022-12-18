a6ca75d2bc21b7695f7850e5076476ee5196bedb
Paul Mauviel <ozzadar@ozzadar.com>
2022-12-18 17:15:28 -0500

    Transforms, Camera movement, better inputs

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <youtube_engine/platform/entry_point.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_access.hpp>
 
 #include <youtube_engine/core/entity.h>
 #include <youtube_engine/core/components/mesh_component.h>
 #include <youtube_engine/core/components/transform_component.h>
+#include <numbers>
 
 using namespace OZZ;
 
@@ -17,51 +19,47 @@ public:
 
         if (_inputManager) {
             // Map inputs
-            _inputManager->MapInputToAction(InputKey::KEY_A, InputAction {
-                .actionName = "strafe",
-                .scale = -1.f
+            _inputManager->MapInputToAction(InputKey::KeyA, InputAction {
+                .ActionName = "strafe",
+                .Scale = -1.f
             });
 
-            _inputManager->MapInputToAction(InputKey::KEY_D, InputAction {
-                    .actionName = "strafe",
-                    .scale = 1.f
+            _inputManager->MapInputToAction(InputKey::KeyD, InputAction {
+                    .ActionName = "strafe",
+                    .Scale = 1.f
             });
-            _inputManager->MapInputToAction(InputKey::GAMEPAD_L_THUMB_X, InputAction {
-                    .actionName = "strafe",
-                    .scale = 1.f
+            _inputManager->MapInputToAction(InputKey::ControllerAxisLeftX, InputAction {
+                    .ActionName = "strafe",
+                    .Scale = 1.f
             });
-            _inputManager->MapInputToAction(InputKey::GAMEPAD_L_THUMB_Y, InputAction {
-                    .actionName = "moveForward",
-                    .scale = 1.f
-            });
-
-            _inputManager->MapInputToAction(InputKey::MOUSE_LEFT, InputAction {
-                .actionName = "click",
-                .scale = 1.f
+            _inputManager->MapInputToAction(InputKey::ControllerAxisLeftY, InputAction {
+                    .ActionName = "moveForward",
+                    .Scale = 1.f
             });
 
-            _inputManager->RegisterActionCallback("strafe", InputManager::ActionCallback {
-                .Ref = "YoutubeGame",
-                .Func = [this](InputSource source, int sourceIndex, float value) {
-//                    std::string direction {"NONE"};
-//
-//                    if (value > 0.f) direction = "RIGHT";
-//                    if (value < 0.f) direction = "LEFT";
-//                    std::cout << "STRAFING " << direction << "\n";
-                    return true;
-                }
+            _inputManager->MapInputToAction(InputKey::KeyW, InputAction {
+                    .ActionName = "moveForward",
+                    .Scale = 1.f
             });
 
-            _inputManager->RegisterActionCallback("moveForward", InputManager::ActionCallback {
-                    .Ref = "YoutubeGame",
-                    .Func = [](InputSource source, int sourceIndex, float value) {
-                        std::string direction {"NONE"};
+            _inputManager->MapInputToAction(InputKey::KeyS, InputAction {
+                    .ActionName = "moveForward",
+                    .Scale = -1.f
+            });
 
-                        if (value > 0.f) direction = "UP";
-                        if (value < 0.f) direction = "DOWN";
-                        std::cout << "MOVING " << direction << "\n";
-                        return true;
-                    }
+            _inputManager->MapInputToAction(InputKey::MouseButtonLeft, InputAction {
+                .ActionName = "click",
+                .Scale = 1.f
+            });
+
+            _inputManager->MapInputToAction(InputKey::MouseMoveY, InputAction {
+                    .ActionName = "lookY",
+                    .Scale = -0.25f
+            });
+
+            _inputManager->MapInputToAction(InputKey::MouseMoveX, InputAction {
+                    .ActionName = "lookX",
+                    .Scale = -0.25f
             });
 
             _inputManager->RegisterActionCallback("click", InputManager::ActionCallback {
@@ -88,40 +86,72 @@ public:
         auto mesh = ServiceLocator::GetResourceManager()->Load<Mesh>("meshes/BasicCube/DiffuseOnly/BasicCube.glb");
         entities[0]->AddComponent<MeshComponent>(std::move(mesh));
         auto& transform = entities[0]->AddComponent<TransformComponent>();
-        transform.Scale *= 0.5f;
+        transform.SetScale(glm::vec3{1.f} * 0.5f);
 
         auto mesh2 = ServiceLocator::GetResourceManager()->Load<Mesh>("meshes/BasicCone.glb");
         entities[1]->AddComponent<MeshComponent>(std::move(mesh2));
         auto& transform2 = entities[1]->AddComponent<TransformComponent>();
-        transform2.Translation = glm::vec3(-2.5f, 0.0f, 0.0f);
+        transform2.SetPosition({-2.5f, 0.0f, 0.0f});
 
         auto mesh3 = ServiceLocator::GetResourceManager()->Load<Mesh>("meshes/BasicCube/DiffuseOnly/BasicCube.glb");
         entities[2]->AddComponent<MeshComponent>(std::move(mesh3));
         auto& transform3 = entities[2]->AddComponent<TransformComponent>();
-        transform3.Scale *= 0.5f;
-        transform3.Translation = glm::vec3(2.5f, 0.0f, 0.0f);
+        transform3.SetScale({0.5, 0.5, 0.5});
+        transform3.SetPosition({2.5f, 0.0f, 0.0f});
 
         auto mesh4 = ServiceLocator::GetResourceManager()->Load<Mesh>("meshes/BasicCone.glb");
         entities[3]->AddComponent<MeshComponent>(std::move(mesh4));
         auto& transform4 = entities[3]->AddComponent<TransformComponent>();
-        transform4.Scale *= 0.5f;
-        transform4.Translation = glm::vec3(0.0, 1.5f, 0.0f);
-        glm::quat rotation = glm::quat(glm::vec3(M_PI / 2, 0, 0.0));
-        transform4.Rotation = transform4.Rotation * rotation;
+        transform4.SetScale(glm::vec3{1.f} * 0.5f);
+        transform4.SetPosition({0.0, 1.5f, 0.0f});
+        glm::quat rotation = glm::quat(glm::vec3(std::numbers::pi / 2, 0, 0.0));
+        transform4.RotateBy(0, 90.f, 0);
+
+        camera = GetScene()->CreateEntity();
+        auto& camTransform = camera->AddComponent<TransformComponent>();
+        camTransform.SetPosition({0, 0, 5});
+        camTransform.RotateBy(180.f);
+
+        auto& camComponent = camera->AddComponent<CameraComponent>();
+        camComponent.SetActive(true);
+        camComponent.FieldOfView = 75.f;
     }
 
 protected:
     void Update(float deltaTime) override {
 
         auto& transform = entities[0]->GetComponent<TransformComponent>();
-
-        glm::quat rotation = glm::quat(glm::vec3(0, ((M_PI * 2) / 3) * deltaTime, 0.0));
-        transform.Rotation = transform.Rotation * rotation;
+        transform.RotateBy((360.f / 3) * deltaTime, 0, 0.0);
 
         auto& transform2 = entities[3]->GetComponent<TransformComponent>();
+        transform2.RotateBy((-360.f / 3) * deltaTime, 0, 0);
 
-        glm::quat rotation2 = glm::quat(glm::vec3(0, 0, ((M_PI * 2) / 3) * deltaTime));
-        transform2.Rotation = transform2.Rotation * rotation2;
+        auto& cameraTransform = camera->GetComponent<TransformComponent>();
+        auto& cameraComponent = camera->GetComponent<CameraComponent>();
+
+        auto moveForwardAmount = _inputManager->GetActionValue("moveForward");
+
+        auto scaledSpeed = movementSpeed * deltaTime;
+        if (std::abs(moveForwardAmount) > 0.05f) {
+            cameraTransform.Translate(MoveDirection::Backward, moveForwardAmount * scaledSpeed);
+        }
+
+        auto strafeAmount = _inputManager->GetActionValue("strafe");
+        if (std::abs(strafeAmount) > 0.05f) {
+            cameraTransform.Translate(MoveDirection::Right, strafeAmount * scaledSpeed);
+        }
+
+        auto scaledLookSpeed = lookSpeed * deltaTime;
+
+        auto lookXAmount = _inputManager->GetActionValue("lookX");
+        if (std::abs(lookXAmount) > 0.05f && std::abs(lookXAmount) < 10.f) {
+            cameraTransform.RotateBy(lookXAmount * scaledLookSpeed, 0.f);
+        }
+
+        auto lookYAmount = _inputManager->GetActionValue("lookY");
+        if (std::abs(lookYAmount) > 0.05f && std::abs(lookYAmount) < 10.f) {
+            cameraTransform.RotateBy(0.f, lookYAmount * scaledLookSpeed, 0, true);
+        }
     }
 
 private:
@@ -132,6 +162,9 @@ private:
     InputManager* _inputManager { nullptr };
 
     std::vector<Entity*> entities {};
+    Entity* camera { nullptr };
+    float movementSpeed = 10.f;
+    float lookSpeed = 180.f;
 };
 
 // Runtime
